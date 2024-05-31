ded5ae8808ad24283165cbe32bfdc81db9fa3f45
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-31 11:23:40 +0530

    wrpeed in a div

--- src/script.js
+++ src/script.js
@@ -5,10 +5,13 @@ async function fetchProducts() {
   const result = await fetch(url);
   const data = await result.json();
   for (const product of data) {
-    products.innerHTML += `<img src="${product.image}">
+    products.innerHTML += `
+    <div class="bg-white p-4 rounded-lg shadow-md">
+    <img src="${product.image}">
         <h2><strong>Title:</strong> ${product.title}</h2>
         <h3><strong>Price: </strong>${product.price}</h3>
         <h3><strong>Price: </strong>${product.price}</h3>
+        </div
     `;
   }
 }
