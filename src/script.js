a1ab2a3e34f1f80286329bf869aa691ac9a13e57
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-31 11:01:59 +0530

    added a div to store products

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
+const url = "https://fakestoreapi.com/products";
+
 async function fetchProducts() {
-  const url = "https://fakestoreapi.com/products";
   const result = await fetch(url);
   const data = await result.json();
   console.log(data);
