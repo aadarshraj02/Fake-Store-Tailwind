c02d36c3e5db22b2166327b963b4ddf020312712
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-31 11:22:01 +0530

    fetched the requied data from api

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
 const url = "https://fakestoreapi.com/products";
+const products = document.querySelector(".products");
 
 async function fetchProducts() {
   const result = await fetch(url);
   const data = await result.json();
-  console.log(data);
+  for (const product of data) {
+    products.innerHTML += `<img src="${product.image}">
+        <h2><strong>Title:</strong> ${product.title}</h2>
+        <h3><strong>Price: </strong>${product.price}</h3>
+        <h3><strong>Price: </strong>${product.price}</h3>
+    `;
+  }
 }
 fetchProducts();
